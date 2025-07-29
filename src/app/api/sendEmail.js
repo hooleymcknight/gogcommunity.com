99c0d44ddc4c3e68370d6985539b359e0159c741
Holly Phillips <hollymphillips.dev@gmail.com>
2025-07-28 21:43:05 -0700

    added email contact us page functionality

---
+++ src/app/api/sendEmail.js
@@ -0,0 +1,17 @@
+export const sendEmail = (data) => {
+    // console.log(data);
+
+    const apiEndpoint = '/api/email';
+
+    fetch(apiEndpoint, {
+        method: 'POST',
+        body: JSON.stringify(data),
+    })
+    .then((res) => res.json())
+    .then((response) => {
+        alert(response.message);
+    })
+    .catch((err) => {
+        alert(err);
+    });
+}
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,24 +1,38 @@
-import Image from "next/image";
+'use client';
 import '../styles/contact.css';
 import Navbar from "../helpers/navbar";
+import { useForm } from 'react-hook-form';
+import { sendEmail } from '../api/sendEmail';
 
-export default async function Contact() {
+export default function Contact() {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        sendEmail(data);
+    }
 
     return (
         <>
             <Navbar />
-            <div className="main-container grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+            <div className="contact main-container grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
                 <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                    <Image
-                        src="/GoG-logo.svg"
-                        alt="GoG Community logo"
-                        width={180}
-                        height={38}
-                        priority
-                    />
-                    <p>contact us</p>
-                    <a href="/">go back</a>
+                    <h1>Contact Us</h1>
 
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="input-group">
+                            <label>Full Name</label>
+                            <input type="text" placeholder="Full Name" {...register('name', {required: true})} />
+                        </div>
+                        <div className="input-group">
+                            <label>Email</label>
+                            <input type="email" placeholder="email@domain.com" {...register('email', {required: true})} />
+                        </div>
+                        <div className="input-group">
+                            <label>Your Message</label>
+                            <textarea rows={4} placeholder="Your message here..." {...register('message', {required: true})} />
+                        </div>
+                        <button id="submitMessage">Submit</button>
+                    </form>
                 </main>
             </div>
         </>
